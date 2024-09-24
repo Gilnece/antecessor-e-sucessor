b368db2956482575c0d9ce1a60e38fd2a1eb623c
Gilnece Menezes <82941591+Gilnece@users.noreply.github.com>
2024-09-24 13:59:42 -0300

    Add antecessor e sucessor
    algoritmo com logica para informar qual o numero antecessor e sucessor do numero desejado!

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>antecessor e sucessor</title>
+</head>
+<body>
+<?php
+$num = 10;
+
+//$ant = - 1;
+//$suc = + 1;
+
+// echo "o antecessor de $num é $ant;
+// echo "o sucessor de $num é $suc;
+
+echo "O numero analisado será: $num<br>";
+echo "O antecessor de $num é ".  $num - 1 ."<br>";
+echo "O sucessor de $num é ".  $num + 1 ."\n";
+
+?>
+
+</body>
+</html>
